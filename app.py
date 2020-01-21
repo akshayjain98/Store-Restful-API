813abdd57c56b99a47453e5c34f87b8557f11ccf
Akshay Jain <jainakshay.aj98@gmail.com>
2020-01-21 18:27:08 +0530

    added flask jwt extender

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
 from flask import Flask
-from Resource.UserRoute import UserRoute
+from Resource.UserRoute import UserRoute, UserByIdRoute, UserLogin
 from Resource.ItemRoute import ItemRoute, ItemRouteById
 from Resource.StoreRoute import StoreRoute, StoreByNameRoute
 from Security.Security import identity, authentication
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 # from Model import CreateTableModel
 from flask_restful import Api
 
@@ -13,21 +13,32 @@ app = Flask(__name__)
 app.secret_key = "AKSHAYJAIN"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///store.db")
+app.config["PROPAGATE_EXCEPTIONS"] = True
 # JWT token generation
-jwt = JWT(app, authentication, identity)
-
+jwt = JWTManager(app)
 
 # creating database with its table using SQLAlchemy
 
 
 api = Api(app)
 
+
 @app.before_first_request
 def create_tables():
     db.create_all()
 
+
+@jwt.user_claims_loader
+def add_more_claims(identity):
+    if identity["role"] == "a":
+        return {"is_admin": True}
+    return {"is_admin": False}
+
+
 # Calling User Route
 api.add_resource(UserRoute, "/user")
+api.add_resource(UserByIdRoute, "/user/<int:user_id>")
+api.add_resource(UserLogin, "/login")
 
 # Calling Item Route
 api.add_resource(ItemRoute, "/item")
